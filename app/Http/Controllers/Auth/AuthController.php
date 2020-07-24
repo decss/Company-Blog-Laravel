b2958b70a5155fbe33bb19ae57823efc6fe2b278
Yaroslav <decss.j@gmail.com>
2020-07-24 15:38:26 +0400

    Fix: auth login field (27)

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,14 +24,14 @@ class AuthController extends Controller
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     protected $loginView;
-    protected $userName = 'login';
+    protected $username = 'login';
 
     /**
      * Where to redirect users after login / registration.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new authentication controller instance.
