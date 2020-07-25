9c8c2b2a5c286b9f6782ed23a898d2f8ab58702a
Yaroslav <decss.j@gmail.com>
2020-07-25 16:52:28 +0400

    Add permissions/roles migrations (29)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -64,7 +64,7 @@ Route::get('logout', 'Auth\AuthController@logout');
 
 
 // Admin section
-Route::group(['prefix' => 'admin', 'middleware' => 'auth',], function() {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 
     Route::get('/', ['uses' => 'Admin\IndexController@index', 'as' => 'adminIndex']);
     Route::resource('/articles', 'Admin\ArticlesController');
