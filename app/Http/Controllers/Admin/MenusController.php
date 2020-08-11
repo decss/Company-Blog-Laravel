0ae7f8429f2b54ed2a05317518c225ee3ad7b414
Yaroslav <decss.j@gmail.com>
2020-08-11 23:29:56 +0400

    Add admin menu store (41)

--- app/Http/Controllers/Admin/MenusController.php
+++ app/Http/Controllers/Admin/MenusController.php
@@ -113,4 +113,14 @@ class MenusController extends AdminController
         return $this->renderOutput();
     }
 
+    public function store(MenusRequest $request)
+    {
+        $result = $this->m_rep->addMenu($request);
+        if (is_array($result) && !empty($result['error'])) {
+            return back()->with($result);
+        }
+
+        return redirect('/admin')->with($result);
+    }
+
 }
