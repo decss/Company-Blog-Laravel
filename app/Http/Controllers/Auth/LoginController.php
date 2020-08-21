a2bf088bcb5f803ac095d83235f50f22a0a80920
Yaroslav <decss.j@gmail.com>
2020-08-21 19:24:46 +0400

    Updated to Laravel 5.5

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,7 +39,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->middleware('guest')->except('logout');
         $this->loginView = config('config.theme') . '.login';
     }
 
