65005c470fa829b535cbc2e1deab5bdb56bfe817
Yaroslav <decss.j@gmail.com>
2020-08-26 19:51:36 +0400

    Fix: Seeds

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -13,15 +13,19 @@ class CategoriesTableSeeder extends Seeder
     {
         $data = [
             [
+                'parent_id' => 0,
                 'title' => 'Блог',
                 'alias' => 'blog',
             ], [
+                'parent_id' => 1,
                 'title' => 'Компьютеры',
                 'alias' => 'computers',
             ], [
+                'parent_id' => 1,
                 'title' => 'Интересное',
                 'alias' => 'iteresting',
             ], [
+                'parent_id' => 1,
                 'title' => 'Советы',
                 'alias' => 'soveti',
             ],
